664ddda15b5e21391f9db0fc515d5b1e2e6ab060
EDEN <issamghled@gmail.com>
2024-02-21 22:40:08 +0100

    4

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -9,40 +9,45 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = *head;
-	dlistint_t *temp = NULL;
-	unsigned int i = 0;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (*head == NULL)
-		return (-1);
-
-	if (index == 0)
 	{
-		*head = current->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
-	}
-
-	while (current != NULL && i < index)
-	{
-		current = current->next;
-		i++;
-	}
-
-	if (current == NULL)
 		return (-1);
-
-	temp = current->prev;
-	temp->next = current->next;
-
-	if (current->next != NULL)
-		current->next->prev = temp;
-
-	free(current);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
 	return (1);
 }

4c3a3ee6af2f1c7a1679bb461b75d4df575d6de4
EDEN <issamghled@gmail.com>
2024-02-21 22:37:57 +0100

    4

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 /*
     Print a square with the character #
     The size of the square must be the first argument 
@@ -14,7 +13,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
